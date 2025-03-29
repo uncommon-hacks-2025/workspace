62a1142e545490f3c662972b1f43c58bd9b8c279
Brandon Kong <kongbrandon0@gmail.com>
2025-03-29 18:35:23 -0500

    feat: revamped login page

--- src/components/login-form.tsx
+++ src/components/login-form.tsx
@@ -1,14 +1,7 @@
 import { cn } from "@/lib/utils"
 import { Button } from "@/components/ui/button"
-import {
-  Card,
-  CardContent,
-  CardDescription,
-  CardHeader,
-  CardTitle,
-} from "@/components/ui/card"
-import { Input } from "@/components/ui/input"
-import { Label } from "@/components/ui/label"
+import { H2, P } from "./typography"
+import Image from "next/image"
 
 export function LoginForm({
   className,
@@ -16,53 +9,38 @@ export function LoginForm({
 }: React.ComponentPropsWithoutRef<"div">) {
   return (
     <div className={cn("flex flex-col gap-6", className)} {...props}>
-      <Card>
-        <CardHeader>
-          <CardTitle className="text-2xl">Login</CardTitle>
-          <CardDescription>
-            Enter your email below to login to your account
-          </CardDescription>
-        </CardHeader>
-        <CardContent>
-          <form>
-            <div className="flex flex-col gap-6">
-              <div className="grid gap-2">
-                <Label htmlFor="email">Email</Label>
-                <Input
-                  id="email"
-                  type="email"
-                  placeholder="m@example.com"
-                  required
+      <div
+      className={'flex flex-col gap-4'}
+      >
+        <div
+        className={'flex flex-col gap-2 items-center'}
+        >
+          <H2>Login to your account</H2>
+            <P className="text-muted-foreground text-center">
+              We take privacy very seriously. We require your consent to proceed.
+            </P>
+        </div>
+        <div
+        className={'text-center'}
+        >
+              
+              <Button className="w-full py-8 border border-transparent hover:border-black transition-colors relative">
+                <Image
+                src={'/logos/auth0.svg'}
+                alt={'Auth0 Logo'}
+                width={32}
+                height={32}
+                className={'absolute left-4'}
                 />
-              </div>
-              <div className="grid gap-2">
-                <div className="flex items-center">
-                  <Label htmlFor="password">Password</Label>
-                  <a
-                    href="#"
-                    className="ml-auto inline-block text-sm underline-offset-4 hover:underline"
-                  >
-                    Forgot your password?
-                  </a>
-                </div>
-                <Input id="password" type="password" required />
-              </div>
-              <Button type="submit" className="w-full">
-                Login
+                Login with Auth0
               </Button>
-              <Button variant="secondary" className="w-full">
-                Login with Google
-              </Button>
-            </div>
-            <div className="mt-4 text-center text-sm">
-              Don&apos;t have an account?{" "}
-              <a href="#" className="underline underline-offset-4">
-                Sign up
-              </a>
-            </div>
-          </form>
-        </CardContent>
-      </Card>
+
+                <Button variant={'link'}>
+                Learn more about how we handle your data.
+                </Button>
+           
+        </div>
+      </div>
     </div>
   )
 }
