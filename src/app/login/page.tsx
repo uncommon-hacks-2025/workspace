18987e7402c61303378a788953a6d752764b93bd
Kiran Velamati <kiranmsd32prince@gmail.com>
2025-03-29 17:32:38 -0500

    login page added

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,15 +1,14 @@
-import { H2 } from "@/components/typography";
+
+import { LoginForm } from "@/components/login-form"
 
 export default function LoginPage() {
     return (
-        <div className="flex flex-row w-full items-center justify-center min-h-screen gap-16 font-[family-name:var(--font-geist-sans)]">
+        <div className="flex min-h-svh w-full items-center justify-center p-6 md:p-10">
             <div
-            className={'w-full max-w-lg text-center'}
+            className={'w-full max-w-sm'}
             >
+               <LoginForm />
                
-               <H2>
-                TEST
-               </H2>
             </div>
         </div>
     )
