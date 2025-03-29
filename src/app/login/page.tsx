6c5732443ed3ec6e2fd1e967ddffaf7f26de56a7
Brandon Kong <kongbrandon0@gmail.com>
2025-03-29 16:48:00 -0500

    feat: added stuff

---
+++ src/app/login/page.tsx
@@ -0,0 +1,16 @@
+import { H2 } from "@/components/typography";
+
+export default function LoginPage() {
+    return (
+        <div className="flex flex-row w-full items-center justify-center min-h-screen gap-16 font-[family-name:var(--font-geist-sans)]">
+            <div
+            className={'w-full max-w-lg text-center'}
+            >
+               
+               <H2>
+                TEST
+               </H2>
+            </div>
+        </div>
+    )
+}
